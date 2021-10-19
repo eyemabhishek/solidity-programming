c9519f3c6a3158c6ff1aaee588e5e4753c90a504
Abhishek Kumar <81435274+eyemabhishek@users.noreply.github.com>
2021-10-19 10:45:30 +0530

    Update MyVariables.sol

--- MyVariables.sol
+++ MyVariables.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.0;
 
+//  State Variables − Variables whose values are permanently stored in a contract storage.
+//  Local Variables − Variables whose values are present till function is executing.
+//  Global Variables − Special variables exists in the global namespace used to get information about the blockchain.
+
+
 contract MyVariables {
     uint256 public number = 20; // state variable
     bool private paused = true; // state variable
